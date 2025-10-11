db6d67d363a9cbbe7041f56cf1288409eb0e6052
Brucesquared2 <bruce@affll.com>
2025-10-10 23:50:56 -0200

    Add modular Bubbles Volume BigBeluga Python module.

---
+++ ta_modules/bubbles_volume_bigbeluga.py
@@ -0,0 +1,14 @@
+"""
+Bubbles Volume BigBeluga (MDPS)
+Settings: [Add matching settings from Pine Script]
+Implements: Volume bubble detection, significant volume levels
+"""
+
+class BubblesVolumeBigBeluga:
+    def __init__(self, threshold=1, levels_qty=10, ...):
+        self.threshold = threshold
+        self.levels_qty = levels_qty
+
+    def calculate(self, price_data, volume_data):
+        # TODO: Implement bubble detection logic
+        pass
