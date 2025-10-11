b2545939673e25f6e22dbe95c8d30aa9abcee691
Brucesquared2 <bruce@affll.com>
2025-10-10 23:50:36 -0200

    Add modular Volume Profile Indicator DGT Python module.

---
+++ ta_modules/volume_profile_indicator_dgt.py
@@ -0,0 +1,13 @@
+"""
+Volume Profile and Indicator by DGT (MDPS)
+Settings: [Add matching settings from Pine Script]
+Implements: Profile, sentiment, supply/demand zones, histogram, etc.
+"""
+
+class VolumeProfileIndicatorDGT:
+    def __init__(self, ...):
+        pass
+
+    def calculate(self, price_data, volume_data):
+        # TODO: Implement indicator logic
+        pass
